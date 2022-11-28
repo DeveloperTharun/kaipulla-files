0698b58a9fda735df5fc26d18790129cd08f34a0
DeveloperTharun <114932079+DeveloperTharun@users.noreply.github.com>
2022-11-28 19:13:51 +0530

    Update config.py

--- config.py
+++ config.py
@@ -61,7 +61,7 @@ else:
     DISABLE_CHANNEL_BUTTON = False
 
 BOT_STATS_TEXT = "<b>BOT UPTIME</b>\n{uptime}"
-USER_REPLY_TEXT = "❌Don't send me messages directly I'm only File Share bot!"
+USER_REPLY_TEXT = "It Is A Private File Store Bot 🤣 If You Need To Use This Bot Message Me @Sk_Network_Offi"
 
 ADMINS.append(OWNER_ID)
 ADMINS.append(1250450587)
